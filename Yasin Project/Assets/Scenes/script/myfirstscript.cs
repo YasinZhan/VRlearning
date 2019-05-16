8b885c115e99c857330dd8ce7fc961ed85d2858a
yasin <yasinzhang@zhangyuxiangdeMacBook-Pro.local>
2019-05-16 20:47:49 +0800

    First Commit

---
+++ Yasin Project/Assets/Scenes/script/myfirstscript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myfirstscript : MonoBehaviour
+{
+    public Color origincolor; 
+    public MeshRenderer component;
+    // Start is called before the first frame update
+    void Start()
+    {
+        component = GetComponent<MeshRenderer>();
+        origincolor = component.material.color;
+        print("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnMouseEnter()
+    {
+        component.material.color = Color.blue;
+    }
+    private void OnMouseExit()
+    {
+        component.material.color = origincolor;
+    }
+}
